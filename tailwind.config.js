fde1c83eebfd812ebd68d3045b3cc608d1c600fb
Yagizalp Zaimoglu <yz@yagi.sh>
2024-03-05 23:46:47 +0100

    Add darkMode to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./templ/**/*.{templ,html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
