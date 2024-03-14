a2d70fedd2dbf4fef9d09eb29d0f35c6758f117c
Yagizalp Zaimoglu <yz@yagi.sh>
2024-03-14 22:34:34 +0100

    Add dashboard page.svelte

---
+++ frontend/src/routes/(authenticated)/dashboard/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import MitochoPage from "@/components/MitochoPage.svelte";
+	import Loading from "@/loading/Loading.svelte";
+
+</script>
+
+<MitochoPage title="Dashboard" description="Manage your sites connected to your Mitocho instance from here">
+  <Loading>
+    <div>Dashboard</div>
+  </Loading>
+</MitochoPage>
